1c615a9583307729e8c31507be2aa1d2a349e352
shanu <shanavaz@doubledevs.com>
2022-06-21 21:12:45 +0530

    Company registration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
+import BootstrapNavbar from './BootstrapNavbar.js';
+import Company from './Components/Company';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BootstrapNavbar/>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Company />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Company.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react'
+import { Form, Button, Table } from 'react-bootstrap'
+import axios from 'axios';
+
+function Company() {
+
+    let [code, setCode] = useState('');
+    let [name, setName] = useState('');
+    let [ceoname, setCeoname] = useState('');
+    let [turnover, setTurnover] = useState('');
+    let [website, setWebsite] = useState('');
+    let [enlist, setEnlist] = useState('');
+
+    const handleChange = (event) => {
+        console.log(event.target.value)
+        setCode(event.target.type);
+        // console.log(code)
+    }
+    const submitForm = () => {
+        var reqData = {
+            code,
+            name,
+            ceo: ceoname,
+            turnover,
+            website,
+            enlistment: enlist
+        }
+
+        console.log(reqData)
+
+        let url = "/api/v1.0/market/company/register"
+
+        axios.post(url, reqData)
+            .then(result => {
+                this.setState({
+                    customer: result.data,
+                    cId: result.data.apiResults[0].id,
+                    name: result.data.apiResults[0].name,
+                    mobile: result.data.apiResults[0].mobile,
+                    email: result.data.apiResults[0].email,
+                })
+                console.log(result.data)
+            }
+            )
+            .catch(error =>
+                console.log(error)
+            );
+    }
+
+    return (
+        <Form>
+            <Table bordered hover>
+                <tbody>
+                    <tr>
+                        <td>Code</td>
+                        <td><Form.Control type="text" onChange={(event)=>{setCode(event.target.value);}} placeholder="Enter code" /></td>
+                    </tr>
+                    <tr>
+                        <td>Name</td>
+                        <td><Form.Control type="text" onChange={(event)=>{setName(event.target.value);}} placeholder="Enter name" /></td>
+                    </tr>
+                    <tr>
+                        <td>CEO</td>
+                        <td><Form.Control type="text" onChange={(event)=>{setCeoname(event.target.value);}} placeholder="Enter ceo name" /></td>
+                    </tr>
+                    <tr>
+                        <td>Turnover</td>
+                        <td><Form.Control type="text" onChange={(event)=>{setTurnover(event.target.value);}} placeholder="Enter Turnover" /></td>
+                    </tr>
+                    <tr>
+                        <td>Website</td>
+                        <td><Form.Control type="text" onChange={(event)=>{setWebsite(event.target.value);}} placeholder="Enter website" /></td>
+                    </tr>
+                    <tr>
+                        <td>Enlistment</td>
+                        <td><Form.Control type="text" onChange={(event)=>{setEnlist(event.target.value);}} placeholder="Enter enlistment" /></td>
+                    </tr>
+                    <tr>
+                        <td colSpan={2}><Button variant="primary" onClick={submitForm}>
+                            Submit
+                        </Button></td>
+                    </tr>
+                </tbody>
+            </Table>
+        </Form>
+    )
+}
+
+export default Company
